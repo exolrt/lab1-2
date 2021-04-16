7510785f74dbe0115f73397bc77e802b1b09dd4f
exolrt <48033886+exolrt@users.noreply.github.com>
2021-04-16 13:04:51 +0300

    Add files via upload

---
+++ Lab1/Form1/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿
+namespace Form1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(218, 147);
+            this.listBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 190);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(97, 45);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 241);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(218, 45);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Stop";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(123, 215);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(107, 20);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.Text = "1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(120, 190);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(110, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Количество потоков";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(250, 296);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Closing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Lab1/Form1/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Threading;
+
+namespace Form1
+{
+    public partial class Form1 : Form
+    {
+        Process Child = null;
+        EventWaitHandle EventStart = new EventWaitHandle(false, EventResetMode.AutoReset, "event_start");
+        EventWaitHandle EventStop = new EventWaitHandle(false, EventResetMode.AutoReset, "event_stop");
+        EventWaitHandle EventConfirm = new EventWaitHandle(false, EventResetMode.AutoReset, "event_confirm");
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private bool console() // проверка запущена ли консоль
+        {
+            if ((Child == null) || (Child.HasExited))
+            { 
+                listBox1.Items.Clear();
+                return false;
+            }
+            return true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (console())
+            {
+                int thread_count = Convert.ToInt32(textBox1.Text); // число с textbox
+                for (int j = 0; j < thread_count; j++)
+                {
+                    EventStart.Set(); // отправка события о создании потока в C++
+                    EventConfirm.WaitOne(); // ожидание подверждения из C++
+                    listBox1.Items.Add("Поток " + (listBox1.Items.Count + 1).ToString() + " создан");
+                }
+            }
+            else
+            {
+                Child = Process.Start("C:/Users/ketak/OneDrive/Рабочий стол/study/системное программирование/Lab1/Debug/Lab1.exe"); 
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (console())
+            {   
+                int num_last_thread = listBox1.Items.Count;
+                EventStop.Set();  // отправка события в с++
+                EventConfirm.WaitOne(); // ожидание события из с++
+                if (num_last_thread > 0)
+                {
+                    listBox1.Items.RemoveAt(num_last_thread - 1); //удаление последней записи из listbox
+                }
+            }
+            else
+            {
+                Child = null;
+                return;
+            }
+        }
+
+        private void Closing(object sender, FormClosingEventArgs e)
+        {
+            if (console())
+            {
+                EventStop.Set();
+                EventConfirm.WaitOne();
+
+            }
+        }   
+    }
+}
